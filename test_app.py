c8f210933e26abab9f3d9a54453ec67ffe17fd72
Wincioor11 <wincioor11@gmail.com>
2020-03-30 23:10:36 +0200

    initial

--- test_app.py
+++ test_app.py
@@ -9,3 +9,10 @@ def test_hello():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
+
+
+# @pytest.mark.parametrize('method', ['get', 'post', 'put', 'delete'])
+def test_method():
+    response = client.get('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
