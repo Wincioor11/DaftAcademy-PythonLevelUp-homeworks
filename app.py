c6fbcf51cce2cc9cd6349e26e12c5ad4f4dab4fd
Wincioor11 <wincioor11@gmail.com>
2020-03-31 00:02:25 +0200

    hw 1.3

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, ValidationError
 
 app = FastAPI()
+
+counter: int = 0
 
 
 @app.get('/')
 def hello():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+class AddPatientRq(BaseModel):
+    name: str
+    surename: str
+
+
+class AddPatientResp(BaseModel):
+    id: int
+    patient: AddPatientRq
+
+
+@app.post('/patient')
+def add_patient(patient: AddPatientRq):
+    global counter
+    counter += 1
+    return AddPatientResp(id=counter, patient=patient)
 
 
 @app.get('/method')
--- test_app.py
+++ test_app.py
@@ -4,6 +4,8 @@ from app import app
 
 client = TestClient(app)
 
+counter = 0
+
 
 def test_hello():
     response = client.get('/')
@@ -16,3 +18,14 @@ def test_method():
     response = client.get('/method')
     assert response.status_code == 200
     assert response.json() == {"method": "GET"}
+
+
+@pytest.mark.parametrize('patient', [{'name': 'John', 'surename': 'Kowalski'}, {'name': 'Żaneta', 'surename': 'Łaput'},
+                                     {'name': 'v678%^&*UHGFR67uhgt67uko98hgef', 'surename': 'HablbaWW__loooll12(()\\'}])
+def test_add_patient(patient):
+    global counter
+    counter += 1
+    response = client.post('/patient', json=patient)
+    assert response.status_code == 200
+    assert response.json() == {"id": counter, "patient": patient}
+
