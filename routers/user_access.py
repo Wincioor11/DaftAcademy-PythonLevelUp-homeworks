252f2fb04bafb4960993fdfb0cfb7fb5f802182a
Wincioor11 <wincioor11@gmail.com>
2020-04-21 10:46:37 +0200

    hw 3.2 add login method v2

--- routers/user_access.py
+++ routers/user_access.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter, Request, HTTPException
-from fastapi.responses import RedirectResponse
+import secrets
 
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette import status
 
 router = APIRouter()
 router.secret_key = 'IK37DHYW92#0?PR1sOHR2311aQLR{[JNCREKCS1@LDNT6YHST389DV.A!1SGKJW4'  # 64 characters 'secret' key
 user = {'login': 'trudnY', 'pass': 'PaC13Nt'}
+
+security = HTTPBasic()
 
 
 @router.get('/welcome')
@@ -18,20 +23,34 @@ def welcome():
 #     password: str
 
 
-@router.post('/login')
-async def login(request: Request):
-    form_body = await request.json()
-    print(form_body)
-    # login = ''
-    # password = ''
-    try:
-        login = form_body['login']
-        password = form_body['pass']
-    except:
-        raise HTTPException(status_code=400, detail="Missing login or pass fields in request body")
+# @router.post('/login')
+# async def login(request: Request):
+#     form_body = await request.json()
+#     print(form_body)
+#     # login = ''
+#     # password = ''
+#     try:
+#         login = form_body['login']
+#         password = form_body['pass']
+#     except:
+#         raise HTTPException(status_code=400, detail="Missing login or pass fields in request body")
+#
+#     if login == user['login'] and password == user['pass']:
+#         return RedirectResponse(url='/welcome', status_code=302)
+#         # return {"message": "POSZŁO!"}
+#     else:
+#         raise HTTPException(status_code=401, detail="Unoauthorized - provide correct login and pass")
 
-    if login == user['login'] and password == user['pass']:
-        return RedirectResponse(url='/welcome', status_code=302)
-        # return {"message": "POSZŁO!"}
-    else:
-        raise HTTPException(status_code=401, detail="Unoauthorized - provide correct login and pass")
+
+@router.post('/login')
+def login(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, user['login'])
+    correct_password = secrets.compare_digest(credentials.password, user['pass'])
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect login or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return RedirectResponse(url='/welcome', status_code=302)
+
